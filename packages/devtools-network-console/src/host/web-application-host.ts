3f5f479fcd94e012c53e187f7a6453dcacac298b
Rob Paveza <ropaveza@microsoft.com>
2020-07-29 09:31:40 -0700

    Targeted perf improvements (#23)
    * Targeted perf improvements
    
    This change implements targeted perf improvements when the number of messages in the WebSocketView is high. This is
    because React would render all the WebSocketMessage components beneath.
    
    Additionally, to improve testability and profiling, this implements a pretty lame "real WebSocket" connection in the
    WebApplicationHost. However, it's pretty unusable, unless someone adds a `connect-src:` declaration to the CSP
    definition in the index.html file.
    
    * Fixed WS mocks

--- packages/devtools-network-console/src/host/web-application-host.ts
+++ packages/devtools-network-console/src/host/web-application-host.ts
@@ -24,9 +24,11 @@ import { DEFAULT_NET_CONSOLE_REQUEST } from 'reducers/request';
 import { synthesizeHttpRequest } from 'utility/http-compose';
 import { recalculateAndApplyTheme } from 'themes/vscode-theme';
 import { INetConsoleRequestInternal } from 'model/NetConsoleRequest';
-import { makeWebsocketMessageLoggedAction } from 'actions/websocket';
+import { makeWebsocketMessageLoggedAction, makeWebSocketDisconnectedAction } from 'actions/websocket';
 
 export default class WebApplicationHost implements INetConsoleHost {
+    private _wsMock: ActualWS | WebSocketMock | null = null;
+
     constructor() {
         setTimeout(() => {
             globalDispatch(setHostCapabilitiesAction(
@@ -44,11 +46,32 @@ export default class WebApplicationHost implements INetConsoleHost {
 
     async makeRequest(request: INetConsoleRequestInternal, environmentalAuthorization: INetConsoleAuthorization | null, environmentVariables: INetConsoleParameter[]): Promise<INetConsoleResponse> {
         if (request.url === 'wss://www.norad.mil/cheyenne/WOPR') {
-            WebSocketMock.instance('wss');
+            this._wsMock = WebSocketMock.instance('DEFAULT_REQUEST');
             const time = Math.random() * 1000;
             setTimeout(() => {
                 globalDispatch(makeWebsocketMessageLoggedAction('DEFAULT_REQUEST', 'recv', Math.floor(time), 'GREETINGS PROFESSOR FALKEN.'));
             }, time);
+            return {
+                duration: 4,
+                status: 'COMPLETE',
+                response: {
+                    headers: [
+                        { key: 'Connection', value: 'Upgrade' },
+                        { key: 'Upgrade', value: 'WebSocket' },
+                    ],
+                    statusCode: 101,
+                    statusText: 'Upgrade',
+                    size: 0,
+                    body: {
+                        content: '',
+                    },
+                },
+            };
+        }
+        else if (request.url.startsWith('wss://')) {
+            const aws = ActualWS.instance('DEFAULT_REQUEST');
+            this._wsMock = aws;
+            aws.connect(request.url);
             return {
                 duration: 4,
                 status: 'COMPLETE',
@@ -133,15 +156,15 @@ export default class WebApplicationHost implements INetConsoleHost {
      * If a connection has been upgraded to a WebSocket, allows it to be disconnected.
      */
     disconnectWebsocket(_requestId: string) {
-        // TODO: Do something here?
+        this._wsMock?.disconnect();
     }
 
     /**
      * If a connection has been upgraded to a WebSocket, sends a message. The default value of
      * the `encoding` parameter is 'text'.
      */
-    sendWebSocketMessage(requestId: string, message: string, _encoding: 'text' | 'base64' = 'text') {
-        WebSocketMock.instance(requestId).send(message);
+    sendWebSocketMessage(_requestId: string, message: string, _encoding: 'text' | 'base64' = 'text') {
+        this._wsMock?.send(message);
     }
 }
 
@@ -235,5 +258,46 @@ export class WebSocketMock {
                 globalDispatch(makeWebsocketMessageLoggedAction(this.requestId, 'recv', Math.floor(Date.now() - this.connected), JSON.stringify({ type: 'ACK', protocol: 'JSON', status: 'OK'})));
             }, 250 + Math.random() * 750);
         }
+    }
+
+    disconnect() {
+        globalDispatch(makeWebSocketDisconnectedAction(this.requestId));
+    }
+}
+
+class ActualWS {
+    private static _instance: ActualWS | null;
+    public static instance(requestId: string) {
+        if (!ActualWS._instance || requestId !== ActualWS._instance.requestId) {
+            ActualWS._instance = new ActualWS(requestId);
+        }
+        return ActualWS._instance;
+    }
+
+    private _ws: WebSocket | null = null;
+    private connected = 0;
+    private constructor(private requestId: string) {
+    }
+
+    connect(url: string) {
+        this._ws = new WebSocket(url);
+        this._ws.addEventListener('open', () => {
+            this.connected = Date.now();
+        });
+        this._ws.addEventListener('message', e => {
+            globalDispatch(makeWebsocketMessageLoggedAction(this.requestId, 'recv', Date.now() - this.connected, e.data));
+        });
+        this._ws.addEventListener('close', () => {
+            globalDispatch(makeWebSocketDisconnectedAction(this.requestId));
+        });
+    }
+
+    send(message: string) {
+        this._ws?.send(message);
+        globalDispatch(makeWebsocketMessageLoggedAction(this.requestId, 'send', Date.now() - this.connected, message));
+    }
+
+    disconnect() {
+        this._ws?.close();
     }
 }
